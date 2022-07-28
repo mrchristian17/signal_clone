b6ff766b20687d29841e25b9ea14bc27cfa420ed
mrchristian17 <dcwrath17@gmail.com>
2022-07-28 14:14:47 -0600

    Initial commit with firebase config

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC50vQRHgfhadsfq_0eutwedm_wgbZYMOQ",
+  authDomain: "signal-clone-d750f.firebaseapp.com",
+  projectId: "signal-clone-d750f",
+  storageBucket: "signal-clone-d750f.appspot.com",
+  messagingSenderId: "570354861751",
+  appId: "1:570354861751:web:96cc3da3bffe299f5aef32"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
