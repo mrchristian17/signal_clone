20827d9e465d763f442e7f281669c9e810a1dbad
mrchristian17 <dcwrath17@gmail.com>
2022-07-29 08:00:58 -0600

    Customized header and added functionality for input to be stored in the database with timestamp, user and message content.

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,14 +1,197 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useLayoutEffect, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Avatar, ListItem } from '@rneui/base';
+import { TouchableOpacity } from 'react-native'
+import { AntDesign, FontAwesome, Ionicons } from '@expo/vector-icons'
+import { SafeAreaView } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { KeyboardAvoidingView } from 'react-native'
+import { Platform } from 'react-native'
+import { ScrollView } from 'react-native'
+import { TextInput } from 'react-native'
+import { Keyboard } from 'react-native'
+import { TouchableWithoutFeedback } from 'react-native'
+import firebase from 'firebase/app'
+import { auth, db } from '../firebase'
+import { addDoc, collection, query, orderBy, onSnapshot, serverTimestamp } from 'firebase/firestore';
+
 
 const ChatScreen = ({ navigation, route }) => {
+
+    const [input, setInput] = useState("")
+    const [messages, setMessages] = useState([])
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            title: "Chat",
+            headerBackTitleVisible: false,
+            headerTitleAlign: "left",
+            headerTitle: () => (
+                <View
+                    style={{
+                        flexDirection: "row",
+                        alignItems: "center",
+                    }} 
+                >
+                    <Avatar rounded source={{ uri: messages[0]?.data.photoURL || "https://connectingcouples.us/wp-content/uploads/2019/07/avatar-placeholder.png" }} />
+                    <Text style={{color: "white", marginLeft: 10, fontWeight: "700"}}>{route.params.chatName}</Text>
+                </View>
+            ),
+            headerLeft: () => (
+                <TouchableOpacity style={{marginLeft: 10}} onPress={navigation.goBack}>
+                     <AntDesign name="arrowleft" size={24} color="white" />
+                </TouchableOpacity>
+            ),
+            headerRight: () => (
+                <View style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    width: 80,
+                    marginRight: 20,
+                }}>
+                    <TouchableOpacity>
+                        <FontAwesome name="video-camera" size={24} color="white" />
+                    </TouchableOpacity>
+                    <TouchableOpacity>
+                        <Ionicons name="call" size={24} color="white" />
+                    </TouchableOpacity>
+                </View>
+            )
+        })
+    },[navigation, messages])
+
+    useLayoutEffect(() => {
+        const q = query(collection(db, 'chats/'+route.params.id+'/messages'), orderBy('timestamp', 'asc'));
+        const unsubscribe = onSnapshot(q, (snapshot) => {
+            setMessages(
+                snapshot.docs.map((doc) => ({
+                    id: doc.id,
+                data: doc.data()
+                }))
+            )
+        })
+        return unsubscribe;
+        // const unsubscribe = db
+        // .collection('chats')
+        // .doc(route.params.id).
+        // collection('messages')
+        // .orderBy('timestamp', 'asc')
+        // .onSnapshot(snapshot => setMessages(
+        //     snapshot.docs.map(doc => ({
+        //         id: doc.id,
+        //         data: doc.data()
+        //     }))
+        // ))
+        // return unsubscribe;
+    }, [])
+
+    const sendMessage = () => {
+        Keyboard.dismiss();
+        addDoc(collection(db, 'chats/'+route.params.id+'/messages'), {
+            timestamp: serverTimestamp(),
+            message: input,
+            displayName: auth.currentUser.displayName,
+            email: auth.currentUser.email,
+            photoURL: auth.currentUser.photoURL
+        })
+        // db.collection('chats').doc(route.params.id).collection('messages').add({
+        //     timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        //     message: input,
+        //     displayName: auth.currentUser.displayName,
+        //     email: auth.currentUser.email,
+        //     photoURL: auth.currentUser.photoURL
+        // })
+        setInput("")
+    }
+
     return (
-        <View>
-            <Text>{route.params.chatname}</Text>
-        </View>
+        <SafeAreaView style={{ flex: 1, backgroundColor: "white"}}>
+            <StatusBar style="light" />
+            <KeyboardAvoidingView 
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={styles.container}
+                keyboardVerticalOffset={90}
+            >
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <>
+                        <ScrollView contentContainerStyle={{paddingTop: 15}}>
+                            {/* Chat */}
+                        </ScrollView>
+                        <View style={styles.footer}>
+                            <TextInput 
+                                value={input}
+                                onChangeText={(text) => setInput(text)}
+                                onSubmitEditing={sendMessage}
+                                style={styles.textInput}
+                                placeholder="Signal Message"
+                            />
+                            <TouchableOpacity onPress={sendMessage} activeOpacity={0.5}>
+                                <Ionicons name="send" size={24} color="#2B68E6" />
+                            </TouchableOpacity>
+                        </View>
+                    </>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
+        </SafeAreaView>
     )
 }
 
-export default ChatScreen;
+export default ChatScreen
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    footer: {
+        flexDirection: "row",
+        alignItems: "center",
+        width: "100%",
+        padding: 15
+    },
+    textInput: {
+        bottom: 0,
+        height: 40,
+        flex: 1,
+        marginRight: 15,
+        backgroundColor: "#ECECEC",
+        padding: 10,
+        color: "grey",
+        borderRadius: 30,
+    },
+    reciever: {
+        padding: 15,
+        backgroundColor: "#ECECEC",
+        alignSelf: "flex-end",
+        borderRadius: 20,
+        marginRight: 15,
+        marginBottom: 20,
+        maxWidth: "80%",
+        position: "relative"
+    },
+    sender : {
+        padding: 15,
+        backgroundColor: "#286BE6",
+        alignSelf: "flex-start",
+        borderRadius: 20,
+        margin: 15,
+        maxWidth: "80%",
+        position: "relative",
+    },
+    senderText: {
+        color: "white",
+        fontWeight: "500",
+        marginLeft: 10,
+        marginBottom: 15,
+    },
+    recieverText: {
+        color: "black",
+        fontWeight: "500",
+        marginLeft: 10,
+    },
+    senderName: {
+        left: 10,
+        paddingRight: 10,
+        fontSize: 10,
+        color: "white",
+    }
+})
